752f350778ac3134cb399bf3fbc35683c2d35b1e
Johan Fortus <114562683+johanfortus@users.noreply.github.com>
2023-10-23 18:07:58 -0400

    sortedFrequency

--- sortedFrequency.js
+++ sortedFrequency.js
@@ -12,3 +12,68 @@
 // sortedFrequency([1,1,2,2,2,2,3],1) // 2
 // sortedFrequency([1,1,2,2,2,2,3],4) // -1
 
+function sortedFrequency(arr, num){
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+
+    let count = 0;
+    while(leftIdx <= rightIdx && count < 15){
+        count++;
+        middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        middleVal = arr[middleIdx];
+
+        if(middleVal < num){
+            leftIdx = middleIdx + 1;
+        }
+        else if(middleVal > num){
+            rightIdx = middleIdx;
+        }
+        else if(middleVal === num){
+            rightIdx = middleIdx - 1;
+        }
+        console.log('iteration', count);
+        console.log('right pointer =>', 'index = ', rightIdx, 'value = ', arr[rightIdx] )
+        console.log('left pointer =>', 'index = ', leftIdx, 'value = ', arr[leftIdx] )
+        console.log('-------------------------------------------------------------')
+
+    }
+    console.log('part 1 => ', leftIdx)
+    let partOne = leftIdx;
+    console.log('')
+    console.log('')
+    console.log('')
+
+
+    leftIdx = 0;
+    rightIdx = arr.length - 1;
+    count = 0;
+    while(leftIdx <= rightIdx && count < 20){
+        count++
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if(middleVal > num){
+            rightIdx = middleIdx - 1;
+        }
+        else if(middleVal < num){
+            leftIdx = middleIdx + 1;
+        }
+        else if(middleVal === num){
+            leftIdx = middleIdx + 1; 
+        }
+        console.log('iteration', count);
+        console.log('right pointer =>', 'index = ', rightIdx, 'value = ', arr[rightIdx] )
+        console.log('left pointer =>', 'index = ', leftIdx, 'value = ', arr[leftIdx] )
+        console.log('-------------------------------------------------------------')
+    }
+    console.log('part 2 => ', rightIdx)
+    let partTwo = rightIdx;
+
+    console.log('')
+
+    let result = partTwo - partOne + 1;
+
+    return result ? result : -1;
+}
